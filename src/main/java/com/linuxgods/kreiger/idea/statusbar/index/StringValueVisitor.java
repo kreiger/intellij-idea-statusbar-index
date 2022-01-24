e68f710ce983a728af0f8a534f07d05c15dee2de
Christoffer Hammarström <christoffer.hammarstrom@linuxgods.com>
2022-01-24 21:35:31 +0100

    StringValueVisitor: Start implementing method calls

--- src/main/java/com/linuxgods/kreiger/idea/statusbar/index/StringValueVisitor.java
+++ src/main/java/com/linuxgods/kreiger/idea/statusbar/index/StringValueVisitor.java
@@ -1,14 +1,21 @@
 package com.linuxgods.kreiger.idea.statusbar.index;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.Notifications;
 import com.intellij.psi.*;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiUtil;
 import com.siyeh.ig.psiutils.ExpressionUtils;
+import com.siyeh.ig.psiutils.MethodCallUtils;
+import com.siyeh.ig.psiutils.TypeUtils;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
+import static com.intellij.notification.NotificationType.INFORMATION;
 import static com.intellij.psi.JavaTokenType.NULL_KEYWORD;
+import static com.intellij.psi.PsiType.*;
 
 public class StringValueVisitor extends JavaElementVisitor {
     private StringBuilder string;
@@ -23,6 +30,102 @@ public class StringValueVisitor extends JavaElementVisitor {
         if (null != element) {
             element.accept(this);
         }
+    }
+
+    @Override
+    public void visitExpression(PsiExpression expression) {
+        var notification = new Notification("Editor notifications",
+                "Status bar index", "Unsupported expression " + expression, INFORMATION);
+        Notifications.Bus.notify(notification, expression.getProject());
+    }
+
+    @Override
+    public void visitMethodCallExpression(PsiMethodCallExpression methodCallExpression) {
+        PsiReferenceExpression methodExpression = methodCallExpression.getMethodExpression();
+        String methodName = methodExpression.getReferenceName();
+        if (methodName == null) return;
+        PsiExpression qualifierExpression = PsiUtil.skipParenthesizedExprDown(methodExpression.getQualifierExpression());
+        PsiExpression[] arguments = methodCallExpression.getArgumentList().getExpressions();
+        if (isToStringMethodCall(methodName, arguments)) {
+            visitElement(qualifierExpression);
+            return;
+        }
+        boolean stringValueOf = MethodCallUtils.isCallToStaticMethod(methodCallExpression, CommonClassNames.JAVA_LANG_STRING, "valueOf", 1);
+        if (stringValueOf) {
+            visitElement(arguments[0]);
+            return;
+        }
+        if (qualifierExpression != null) {
+            PsiType type = qualifierExpression.getType();
+            if (TypeUtils.isJavaLangString(type)) {
+                visitStringMethodCall( methodCallExpression);
+                return;
+            }
+        }
+        visitExpression(methodCallExpression);
+    }
+
+    private void visitStringMethodCall(PsiMethodCallExpression stringMethodCall) {
+        PsiExpression[] arguments = stringMethodCall.getArgumentList().getExpressions();
+        PsiReferenceExpression methodExpression = stringMethodCall.getMethodExpression();
+        PsiExpression qualifier = methodExpression.getQualifierExpression();
+        String methodName = methodExpression.getReferenceName();
+        if ("concat".equals(methodName) && arguments.length == 1) {
+            visitElement(qualifier);
+            visitElement(arguments[0]);
+            return;
+        }
+        if ("intern".equals(methodName)) {
+            visitElement(qualifier);
+            return;
+        }
+        if ("repeat".equals(methodName) && arguments.length == 1) {
+            getLiteralInteger(arguments[0]).ifPresent(value -> {
+                for (int i = 0; i < (int) value; i++) {
+                    visitElement(qualifier);
+                }
+            });
+        };
+        if ("replace".equals(methodName) && arguments.length == 2
+                && CHAR.equals(arguments[0].getType()) && CHAR.equals(arguments[1].getType())) {
+            visitElement(qualifier);
+            return;
+        }
+        // TODO
+        if ("substring".equals(methodName)) {
+            valueToString(qualifier)
+                    .ifPresent(string -> {
+                        getSubstringEndIndex(arguments, string.length())
+                                .ifPresent(endIndex ->
+                                        getLiteralInteger(arguments[0])
+                                                .ifPresent(beginIndex ->
+                                                        append(string.substring(beginIndex, endIndex))));
+                    });
+        }
+    }
+
+    private Optional<Integer> getSubstringEndIndex(PsiExpression[] arguments, int length) {
+        return arguments.length == 2 ? getLiteralInteger(arguments[1]) : Optional.of(length);
+    }
+
+    private Optional<Object> getArgumentLiteralValue(PsiExpression argument) {
+        if (argument instanceof PsiLiteralExpression) {
+            return Optional.ofNullable(((PsiLiteralExpression) argument).getValue());
+        }
+        return Optional.empty();
+    }
+    private Optional<Integer> getLiteralInteger(PsiExpression expression) {
+        return getArgumentLiteralValue(expression)
+                .filter(value -> value instanceof Integer)
+                .map(value -> (int)value);
+    }
+
+
+    private boolean isToStringMethodCall(@NonNls String methodName, PsiExpression[] arguments) {
+        if ("toString".equals(methodName)) {
+            return arguments.length == 0;
+        }
+        return false;
     }
 
     @Override public void visitReferenceExpression(PsiReferenceExpression expression) {
@@ -48,11 +151,11 @@ public class StringValueVisitor extends JavaElementVisitor {
             append(value);
             return;
         }
-        visitExpression(variable.getInitializer());
+        visitElement(variable.getInitializer());
     }
 
     @Override public void visitParenthesizedExpression(PsiParenthesizedExpression parenthesizedExpression) {
-        visitExpression(PsiUtil.skipParenthesizedExprDown(parenthesizedExpression));
+        visitElement(PsiUtil.skipParenthesizedExprDown(parenthesizedExpression));
     }
 
     @Override public void visitPolyadicExpression(PsiPolyadicExpression polyadicExpression) {
@@ -65,7 +168,7 @@ public class StringValueVisitor extends JavaElementVisitor {
             return;
         }
         for (PsiExpression operand : polyadicExpression.getOperands()) {
-            visitExpression(operand);
+            visitElement(operand);
         }
     }
 
